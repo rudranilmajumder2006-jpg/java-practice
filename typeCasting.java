5c729d0fd43cce0bc00722ef5a8f57620da2ba56
Rudranil <rudranilmajumder2006@gmail.com>
2026-04-18 23:28:39 +0530

    basic code of typecasting in java

---
+++ typeCasting.java
@@ -0,0 +1,14 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b=(byte)a;
+        System.out.println(b);
+        int c = 1000000;
+        byte d=(byte)c;
+        System.out.println(d);
+        double e = 12345.654;
+        float g =(float)e;
+        System.out.println(g);
+
+    }
+}
