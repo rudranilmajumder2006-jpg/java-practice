3379aeaf7d9c3f4ce102712411d29362a2cdae57
Rudranil <rudranilmajumder2006@gmail.com>
2026-04-21 23:53:07 +0530

    basic syntax of array

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args) {
+        int arr[]={1,3,4,5,6};
+        for(int i=0;i<6;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
